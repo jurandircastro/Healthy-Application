d41d15307f82fd621acb6b65f550e382c68a15a4
GleydsonBernardo <gleydson.bernardo@gmail.com>
2016-10-31 13:12:48 -0300

    Criação do projeto + Rascunho do Login
    Primeiro commit

---
+++ app/src/main/java/healthy_application/views/LoginActivity.java
@@ -0,0 +1,15 @@
+package healthy_application.views;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import br.com.dreamteam.healthy_application.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
